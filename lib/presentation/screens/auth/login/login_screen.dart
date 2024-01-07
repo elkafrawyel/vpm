17fab419ea0c239cd2bb78a81b603d31fece2417
mac <mahmoud.ashraf.elkafrawy@gmail.com>
2024-01-07 17:12:01 +0200

    working on design

--- lib/presentation/screens/auth/login/login_screen.dart
+++ lib/presentation/screens/auth/login/login_screen.dart
@@ -41,104 +41,107 @@ class _LoginScreenState extends State<LoginScreen> {
         centerTitle: false,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Hero(
-                tag: 'logo',
-                child: Image.asset(
-                  Res.logoImage,
-                  height: 200,
-                  width: 300,
-                  fit: BoxFit.cover,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Hero(
+                  tag: 'logo',
+                  child: Image.asset(
+                    Res.logoImage,
+                    height: 200,
+                    width: 400,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18.0),
-              child: AppText(
-                'login_to_your_account'.tr,
-                fontSize: 18,
-                fontWeight: FontWeight.w700,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: AppText(
+                  'login_to_your_account'.tr,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-            10.ph,
-            AppTextFormField(
-              key: _emailState,
-              controller: emailController,
-              validateEmptyText: 'email_required'.tr,
-              hintText: 'email'.tr,
-              horizontalPadding: 18,
-              autoFillHints: const [AutofillHints.email],
-              radius: kRadius,
-              appFieldType: AppFieldType.email,
-              prefixIcon: Res.iconEmail,
-            ),
-            AppTextFormField(
-              key: _passwordState,
-              controller: passwordController,
-              validateEmptyText: 'password_required'.tr,
-              hintText: 'password'.tr,
-              horizontalPadding: 18,
-              autoFillHints: const [AutofillHints.email],
-              radius: kRadius,
-              appFieldType: AppFieldType.password,
-              prefixIcon: Res.iconPassword,
-            ),
-            10.ph,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18.0),
-              child: Row(
-                children: [
-                  Checkbox.adaptive(
-                    fillColor: MaterialStateProperty.all(
-                        Theme.of(context).scaffoldBackgroundColor),
-                    checkColor: Theme.of(context).primaryColor,
-                    value: rememberMe,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        rememberMe = value;
-                      });
+              10.ph,
+              AppTextFormField(
+                key: _emailState,
+                controller: emailController,
+                validateEmptyText: 'email_required'.tr,
+                hintText: 'email'.tr,
+                horizontalPadding: 18,
+                autoFillHints: const [AutofillHints.email],
+                radius: kRadius,
+                appFieldType: AppFieldType.email,
+                prefixIcon: Res.iconEmail,
+              ),
+              AppTextFormField(
+                key: _passwordState,
+                controller: passwordController,
+                validateEmptyText: 'password_required'.tr,
+                hintText: 'password'.tr,
+                horizontalPadding: 18,
+                autoFillHints: const [AutofillHints.email],
+                radius: kRadius,
+                appFieldType: AppFieldType.password,
+                prefixIcon: Res.iconPassword,
+              ),
+              10.ph,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Row(
+                  children: [
+                    Checkbox.adaptive(
+                      fillColor: MaterialStateProperty.all(
+                          Theme.of(context).scaffoldBackgroundColor),
+                      checkColor: Theme.of(context).primaryColor,
+                      value: rememberMe,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          rememberMe = value;
+                        });
+                      },
+                    ),
+                    AppText(
+                      'remember_me'.tr,
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                    ),
+                  ],
+                ),
+              ),
+              10.ph,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                  child: AppProgressButton(
+                    text: 'login'.tr,
+                    width: MediaQuery.sizeOf(context).width,
+                    radius: kRadius,
+                    onPressed: (animationController) async {
+                      _login(animationController);
                     },
                   ),
-                  AppText(
-                    'remember_me'.tr,
+                ),
+              ),
+              20.ph,
+              InkWell(
+                onTap: () {
+                  Get.to(() => const ForgetPasswordScreen());
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                  child: AppText(
+                    'forget_password?'.tr,
                     color: Theme.of(context).primaryColor,
                     fontSize: 16,
                   ),
-                ],
-              ),
-            ),
-            10.ph,
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                child: AppProgressButton(
-                  text: 'login'.tr,
-                  width: MediaQuery.sizeOf(context).width,
-                  radius: kRadius,
-                  onPressed: (animationController) async {
-                    _login(animationController);
-                  },
                 ),
-              ),
-            ),
-            20.ph,
-            InkWell(
-              onTap: () {
-                Get.to(() => const ForgetPasswordScreen());
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                child: AppText(
-                  'forget_password?'.tr,
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 16,
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
